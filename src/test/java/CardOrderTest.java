6d3bd210d4cfbdf5ef1da24efabb47455dc70d11
Oleg Titkin <o.a.t@inbox.ru>
2022-10-05 21:44:50 +0300

    add test

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,7 +1,5 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -24,8 +22,16 @@ public class CardOrderTest {
        driver.quit();
        driver = null;
     }
-    @Test void test1(){
+    @Test void shouldFillingPersonalData(){
         driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Старк Тони");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997775555");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        Assertions.assertEquals(expected, actual);
     }
 
 }
